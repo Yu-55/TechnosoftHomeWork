d242b4ca19f19a7f9ecf95b4f93e1d5d55b9a9c8
Yura Kutovoy <yurakty@gmail.com>
2021-02-23 01:12:06 -0500

    My homework

---
+++ HomeWork2/PrimitiveDataTapes.java
@@ -0,0 +1,16 @@
+package HomeWork2;
+
+public class PrimitiveDataTapes {
+
+    public static void main(String[] args) {
+        byte hoursInDay = 24;
+        short maxDaysInYear = 366;
+        int numbersOfEmployees = 135000; // number of Google's employees
+        int usPopulation = 331000000;
+        float interestRate = 1.5f;
+        int bankAccountBalance = 200000;
+        boolean isSunRiseFromWest = false;
+        char myFirstInitial = 'Y'; char mySecondInitial = 'K';
+
+    }
+}
