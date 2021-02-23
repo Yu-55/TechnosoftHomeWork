250c463f0ff9460352e27e77f93a6bd92f1fbf1d
Yura Kutovoy <yurakty@gmail.com>
2021-02-23 17:15:45 -0500

    Some minor changes

--- HomeWork2/PrimitiveDataTapes.java
+++ HomeWork2/PrimitiveDataTapes.java
@@ -5,12 +5,11 @@ public class PrimitiveDataTapes {
     public static void main(String[] args) {
         byte hoursInDay = 24;
         short maxDaysInYear = 366;
-        int numbersOfEmployees = 135000; // number of Google's employees
-        int usPopulation = 331000000;
+        int numbersOfEmployees = 135_000; // number of Google's employees
+        int usPopulation = 331_000_000;
         float interestRate = 1.5f;
-        int bankAccountBalance = 200000;
+        int bankAccountBalance = 200_000;
         boolean isSunRiseFromWest = false;
-        char myFirstInitial = 'Y'; char mySecondInitial = 'K';
-
+        char myFirstInitial = 'Y', mySecondInitial = 'K';
     }
 }
