69cfdd06af73922edfd4e4f6d96705eda4548bb6
Yura Kutovoy <yurakty@gmail.com>
2021-02-23 23:21:46 -0500

    Added print in terminal

--- HomeWork2/PrimitiveDataTapes.java
+++ HomeWork2/PrimitiveDataTapes.java
@@ -4,12 +4,20 @@ public class PrimitiveDataTapes {
 
     public static void main(String[] args) {
         byte hoursInDay = 24;
+        System.out.println("Number of hours in a day is " + hoursInDay);
         short maxDaysInYear = 366;
+        System.out.println("Max number of days in an year is " + maxDaysInYear);
         int numbersOfEmployees = 135_000; // number of Google's employees
+        System.out.println("Total number of employees in an organization is " + numbersOfEmployees);
         int usPopulation = 331_000_000;
+        System.out.println("The population in a country is " + usPopulation);
         float interestRate = 1.5f;
+        System.out.println("Interest rate is " + interestRate);
         int bankAccountBalance = 200_000;
+        System.out.println("Balance in a bank account is " + bankAccountBalance);
         boolean isSunRiseFromWest = false;
+        System.out.println("Does the sun rise from the west? " + isSunRiseFromWest);
         char myFirstInitial = 'Y', mySecondInitial = 'K';
+        System.out.println("Initials of my name is " + myFirstInitial + mySecondInitial);
     }
 }
